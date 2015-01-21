d4fa39fea96ad6938c221c0ee9eb5ab43ea2c87c
alexander zahorskyi <zagorskyalexander@gmail.com>
2015-01-21 21:38:50 +0100

    Created empty library project

---
+++ AZGameEngine/AZGameEngine.h
@@ -0,0 +1,13 @@
+//
+//  AZGameEngine.h
+//  AZGameEngine
+//
+//  Created by Oleksandr Zahorskyi on 21/01/15.
+//  Copyright (c) 2015 Alexander Zahorskyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AZGameEngine : NSObject
+
+@end
---
+++ AZGameEngine/AZGameEngine.m
@@ -0,0 +1,13 @@
+//
+//  AZGameEngine.m
+//  AZGameEngine
+//
+//  Created by Oleksandr Zahorskyi on 21/01/15.
+//  Copyright (c) 2015 Alexander Zahorskyi. All rights reserved.
+//
+
+#import "AZGameEngine.h"
+
+@implementation AZGameEngine
+
+@end
